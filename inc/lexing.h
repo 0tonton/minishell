623d65fd9cfe8ba43e1c6b699a0d3d5ff83efa74
0tonton <ruvik.castellano@gmail.com>
2025-02-16 15:50:52 +0100

    norme

--- inc/lexing.h
+++ inc/lexing.h
@@ -6,14 +6,14 @@
 /*   By: oloncle <oloncle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:33:26 by oloncle           #+#    #+#             */
-/*   Updated: 2025/02/16 13:16:10 by oloncle          ###   ########.fr       */
+/*   Updated: 2025/02/16 15:50:36 by oloncle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXING_H
 # define LEXING_H
 
-//STRUCT
+/*------------------------------STRUCT------------------------------*/
 
 typedef enum e_token_type
 {
@@ -32,54 +32,51 @@ typedef enum e_token_type
 typedef struct s_lexer
 {
 	t_token_type		tok_type;
-	char			*str;
-
-	struct s_lexer	*next;
-	struct s_lexer	*prev;
+	char				*str;
+	struct s_lexer		*next;
+	struct s_lexer		*prev;
 }	t_lexer;
 
+/*------------------------------FUNCTIONS------------------------------*/
 
-//FCTS
-	//////LEXER//////
+	/*LEXER*/
 t_lexer	**lexer_line(char *line, int *e_status, char **env);
 void	add_lexer_node(t_lexer *first_node, char *value, t_token_type tok_type);
 void	free_lexer(t_lexer **first_node);
 
-	//identifying token 
+/*identifying token*/
 void	if_var(t_lexer *first_node, char *line, int *i);
 void	if_sentence(t_lexer *first_node, char *line, int *i);
 void	if_quote(t_lexer *first_node, char *line, int *i);
 void	if_space(t_lexer *first_node, char *line, int *i);
 void	if_operator(t_lexer *first_node, char *line, int *i);
 
-	//handle quotes
-int	handle_quotes(t_lexer **first_node, int e_status, char **env);
+/*handle quotes*/
+int		handle_quotes(t_lexer **first_node, int e_status, char **env);
 void	create_and_add_new_node(t_lexer **first_node, t_lexer *first_quote);
 
-	//handle heredoc
-int	manage_hd(t_lexer **head);
-int	check_dless(t_lexer *head);
+/*handle heredoc*/
+int		manage_hd(t_lexer **head);
+int		check_dless(t_lexer *head);
 
-	//heredoc utils
+/*heredoc utils*/
 char	*find_delim(t_lexer *head);
-int	check_delim_char(t_lexer *node);
+int		check_delim_char(t_lexer *node);
 t_lexer	*dless_node(t_lexer *head);
 void	add_new_node_filename(t_lexer **head, int nb_hd);
 char	*create_filename(int nb_hd);
 
-	//handle env var
+/*handle env var*/
 void	handle_env_var(t_lexer *start, t_lexer *end, int *e_status, char **env);
 char	*ft_getenv(char *name, char **env);
 
-	//check lexer
-int	check_lex_special_char(t_lexer *lex, int *exit_s);
-int	check_lex_all_spaces(t_lexer *head);
+/*check lexer*/
+int		check_lex_special_char(t_lexer *lex, int *exit_s);
+int		check_lex_all_spaces(t_lexer *head);
 
-	//utils
-int	ft_is_space(char c);
-int	is_special(char c);
+/*utils*/
+int		ft_is_space(char c);
+int		is_special(char c);
 char	*ft_itoa(int n);
-
-void	print_lexer_lst(t_lexer **first_node);
 
 #endif
--- inc/parsing.h
+++ inc/parsing.h
@@ -6,14 +6,14 @@
 /*   By: oloncle <oloncle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 11:50:10 by oloncle           #+#    #+#             */
-/*   Updated: 2025/02/16 14:21:19 by oloncle          ###   ########.fr       */
+/*   Updated: 2025/02/16 15:49:37 by oloncle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-// #include "lexing.h"
+/*------------------------------STRUCT------------------------------*/
 
 enum e_type
 {
@@ -26,61 +26,60 @@ typedef struct s_node
 	enum e_type	type; //CMD or PIPE
 }	t_node;
 
-typedef	struct s_cmd_node
+typedef struct s_cmd_node
 {
 	enum e_type	type; // 0
-	char	**cmd_name; //liste du nom de la cmd avec ses arguments, fini par NULL
-
-	char	*input; //NULL if stdin, sinon nom du fichier
-	char	*output; //NULL si stdout, sinon nom du fichier
-	int	append_mode; //0 si pas d'append, et 1 si append au nom de fichier output
-	int	heredoc; //0 si pas here doc, 1 si here doc (nom du fichier en input)
+	char		**cmd_name; //liste de la cmd avec ses arguments, fini par NULL
+	char		*input; //NULL if stdin, sinon nom du fichier
+	char		*output; //NULL si stdout, sinon nom du fichier
+	int			append_mode; //0 si pas d'append, sinon 1
+	int			heredoc; //0 si pas here doc, sinon 1
 }	t_cmd_node;
 
 typedef struct s_pipe_node
 {
 	enum e_type	type; //1
-
-	t_node	*right; //soit pipe_node, soit cmd_node
-	t_node	*left; //soit pipe_node, soit cmd_node
+	t_node		*right; //soit pipe_node, soit cmd_node
+	t_node		*left; //soit pipe_node, soit cmd_node
 }	t_pipe_node;
 
-typedef	struct	s_data
+typedef struct s_data
 {
 	t_node	*head;
 	char	**env;
-	int	exit_status;
-	int	act_pipe_fd0;
-	int	prev_pipe_fd0;
+	int		exit_status;
+	int		act_pipe_fd0;
+	int		prev_pipe_fd0;
 }	t_data;
 
-//FCTS
+/*------------------------------FUNCTIONS------------------------------*/
+/*Creating ast & free*/
+t_node		*creating_tree(t_lexer **head);
+void		free_ast(t_node *top_node);
+void		free_parsing(t_lexer **lex_lst, t_data *data, char *line, \
+int before_exec);
+void		del_hdfiles(void);
 
-t_node	*creating_tree(t_lexer **head);
-void	free_ast(t_node *top_node);
-void	free_parsing(t_lexer **lex_lst, t_data *data, char *line, int before_exec);
-void	del_hdfiles(void);
+/*LIBFT utils*/
+int			is_in_str(char c, char *str);
+int			start_i(char const *s, char *c);
+void		end_var(char const *s, char *c, int *end);
+char		**ft_split(char const *s, char *c);
+int			ft_atoi(const char *nptr);
 
-//LIBFT utils
-int	is_in_str(char c, char *str);
-int	start_i(char const *s, char *c);
-void	end_var(char const *s, char *c, int *end);
-char	**ft_split(char const *s, char *c);
-int	ft_atoi(const char *nptr);
+/*cmd nodes*/
+t_node		*create_cmd_node(t_lexer *first_lex_node);
+int			if_chevron(t_lexer *current, t_cmd_node *cmd_node);
 
-//cmd nodes
-t_node	*create_cmd_node(t_lexer *first_lex_node);
-int	if_chevron(t_lexer *current, t_cmd_node *cmd_node);
-
-//pipe nodes
+/*pipe nodes*/
 t_pipe_node	*creating_n_linked_pipe_nodes(int n);
-int	count_and_check_pipes(t_lexer **head);
+int			count_and_check_pipes(t_lexer **head);
 
-//Env variables
-char	**tab_var_env(char **og_env);
-int	len_var(char *var);
+/*Env variables*/
+char		**tab_var_env(char **og_env);
+int			len_var(char *var);
 
-//parsing utils
-int	count_cmd(t_node *head_node);
+/*parsing utils*/
+int			count_cmd(t_node *head_node);
 
 #endif
